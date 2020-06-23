3d4317b6959209f21f219c891e44d0af8a2cd3fc
Brian Tate <brian.tate@cvisionai.com>
2020-06-23 16:22:06 -0400

    Add notional station model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Station(models.Model):
+    name = models.CharField(max_length=80)
+    last_updated = models.DateTimeField(blank=True,null=True)
+    space_available = models.FloatField(blank=True,null=True)
