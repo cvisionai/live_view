25a03697cec8a2062d2c551ea4482400cf9add85
Brian Tate <brian.tate@cvisionai.com>
2021-11-15 14:51:30 +0000

    [Migrate-Required] Add cell status to station model.

--- main/models.py
+++ main/models.py
@@ -8,10 +8,12 @@ class Station(models.Model):
     last_updated = models.DateTimeField(blank=True,null=True)
     last_image = models.DateTimeField(blank=True,null=True)
     space_available = models.FloatField(blank=True,null=True)
+    cell_percentage = models.FloatField(blank=True,null=True)
+    cell_status = models.CharField(max_length=80, blank=True,null=True)
     image = models.FileField(null=True, blank=True)
     maintenance_required = models.IntegerField(default=0)
     version = models.CharField(max_length=80, blank=True,null=True)
     def __str__(self):
-        return f"{self.name} | {self.pk} | {self.maintenance_required} | {self.last_updated}"
+        return f"{self.name} | {self.pk} | {self.maintenance_required} | {self.last_updated} | {self.cell_percentage}"
 
 admin.site.register(Station)
