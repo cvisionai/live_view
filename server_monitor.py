1490a4f3295f11ea3f6f9c263e99980e09f7e2f5
Brian Tate <brian.tate@cvisionai.com>
2024-02-22 13:52:14 +0000

    Switch SMS alert API to use twilio instead of AWS.

--- server_monitor.py
+++ server_monitor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import boto3
 import argparse
 import time
 import yaml
@@ -9,6 +8,16 @@ from dateutil.parser import parse
 import pytz
 
 from collections import defaultdict
+from twilio.rest import Client
+
+def send_alert(config, message):
+  if not  'twilio'  in  config:
+    print("Twilio alert is not configured!")
+  
+  client = Client(config['twilio']['sid'], config['twilio']['token'])
+  source=config['twilio']['source']
+  for dest in config['notification_numbers']:
+    client.messages.create(from_=f"+{source}", body=message, to=f"+{dest}")
 
 if __name__=="__main__":
   parser = argparse.ArgumentParser()
@@ -18,7 +27,6 @@ if __name__=="__main__":
   with open(args.config, 'r') as fp:
     config = yaml.safe_load(fp)
 
-  print("Publishing to " + config.get('arn'))
   host = config.get('host')
   token = config.get('token')
   server_names = config.get('server_names')
@@ -37,23 +45,19 @@ if __name__=="__main__":
        delta = utc_now - last_hb
        name = data['name']
        time_since_last_alert = utc_now - server_last[server_name]
-       if delta > datetime.timedelta(hours=1):
+       if delta > datetime.timedelta(hours=1) or data['space_available'] < 0:
          if time_since_last_alert > datetime.timedelta(hours=6):
            server_last[server_name] = utc_now
-           msg=f"{name} if out of communication. Last update at {last_hb}"
+           msg=f"Alert: {name} is out of communication. Last update at {last_hb}"
            print(msg)
-           sns = boto3.resource('sns', region_name=config.get('region','us-east-1'))
-           topic = sns.Topic(config.get('arn'))
-           topic.publish(Message=msg)
+           send_alert(config, msg)
            server_bad[server_name] = True
-       elif server_bad[server_name] and delta < datetime.timedelta(hours=1):
+       elif server_bad[server_name] and delta < datetime.timedelta(hours=1) and data['space_available'] > 0:
          server_bad[server_name] = False
          last_alert = pytz.utc.localize(datetime.datetime.fromtimestamp(0))
-         msg=f"{name} is back in range. Last update at {last_hb}"
+         msg=f"Alert: {name} is back in range. Last update at {last_hb}"
          print(msg)
-         sns = boto3.resource('sns', region_name=config.get('region','us-east-1'))
-         topic = sns.Topic(config.get('arn'))
-         topic.publish(Message=msg)
+         send_alert(config,msg)
        else:
          pass
 
